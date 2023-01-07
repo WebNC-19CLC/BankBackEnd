4cb09e06916ee28c2275f889dbd36d21320e4336
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2023-01-07 17:29:24 +0700

    add: edit bank

---
+++ AsrTool/Infrastructure/MediatR/Businesses/Bank/Command/CreateBankCommandAuthorizer.cs
@@ -0,0 +1,31 @@
+﻿using AsrTool.Dtos;
+using AsrTool.Infrastructure.Auth;
+using AsrTool.Infrastructure.Context;
+using AsrTool.Infrastructure.MediatR.Common.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AsrTool.Infrastructure.MediatR.Businesses.Admin.Queries
+{
+  public class GetEmployeeQueryAuthorizer : IAuthorizer<GetEmployeeQuery>
+  {
+    private readonly IUserResolver _userResolver;
+
+    public GetEmployeeQueryAuthorizer(IUserResolver userResolver)
+    {
+      _userResolver = userResolver;
+    }
+
+    public async Task<AuthorizationResult> AuthorizeAsync(GetEmployeeQuery instance, CancellationToken cancellation = default)
+    {
+      if (_userResolver.CurrentUser.RoleName == Constants.Roles.Admin)
+      {
+        return AuthorizationResult.Success();
+      }
+
+      else return AuthorizationResult.Failed();
+    }
+  }
+}
