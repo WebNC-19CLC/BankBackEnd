9b9d21ca81b08326eae1b527fe632d3b965952bd
Mai Hoang Anh <56768363+mrfatchicken@users.noreply.github.com>
2023-01-12 10:20:29 +0700

    add more api doc

--- AsrTool/Swagger/Account/RecipientExample.cs
+++ AsrTool/Swagger/Account/RecipientExample.cs
@@ -3,17 +3,17 @@ using AsrTool.Dtos;
 
 namespace AsrTool.Swagger.Account
 {
-    public class RecipientExample : IMultipleExamplesProvider<RecipientDto>
-    {
+  public class RecipientExample : IMultipleExamplesProvider<RecipientDto>
+  {
 
-        public class ListRecipientExample : IMultipleExamplesProvider<List<RecipientDto>>
-        {
-            public IEnumerable<SwaggerExample<List<RecipientDto>>> GetExamples()
-            {
-                yield return SwaggerExample.Create(
-                  "Example",
-                  new List<RecipientDto>
-                  {
+    public class ListRecipientExample : IMultipleExamplesProvider<List<RecipientDto>>
+    {
+      public IEnumerable<SwaggerExample<List<RecipientDto>>> GetExamples()
+      {
+        yield return SwaggerExample.Create(
+          "Example",
+          new List<RecipientDto>
+          {
                   new RecipientDto
                   {
                       AccountNumber = "123456",
@@ -42,78 +42,93 @@ namespace AsrTool.Swagger.Account
                       Id = 4,
                       SuggestedName = "Pham Van A"
                   },
-                  }
-                );
-            }
-        }
-
-
-        public class CreateRecipientRequestExample : IMultipleExamplesProvider<CreateRecipientDto>
-        {
-            public IEnumerable<SwaggerExample<CreateRecipientDto>> GetExamples()
-            {
-                yield return SwaggerExample.Create(
-                  "With Suggested Name",
-                  new CreateRecipientDto
-                  {
-                      AccountNumber = "345678",
-                      BankDestinationId = null,
-                      SuggestedName = "MyGod",
-                  }
-                );
-
-                yield return SwaggerExample.Create(
-                   "Without Suggested Name",
-                   new CreateRecipientDto
-                   {
-                       AccountNumber = "345678",
-                       BankDestinationId = null,
-                       SuggestedName = null,
-                   }
-                 );
-            }
-        }
-
-        public class CreateRecipientResponseExample : IMultipleExamplesProvider<RecipientDto>
-        {
-            public IEnumerable<SwaggerExample<RecipientDto>> GetExamples()
-            {
-                yield return SwaggerExample.Create(
-                  "With Suggested Name",
-                  new RecipientDto
-                  {
-                      Id = 1,
-                      AccountNumber = "345678",
-                      BankDestinationId = null,
-                      SuggestedName = "MyGod",
-                  }
-                );
-
-                yield return SwaggerExample.Create(
-                   "Without Suggested Name",
-                   new RecipientDto
-                   {
-                       Id = 1,
-                       AccountNumber = "345678",
-                       BankDestinationId = null,
-                       SuggestedName = "Nguyen Van B",
-                   }
-                 );
-            }
-        }
-
-        public IEnumerable<SwaggerExample<RecipientDto>> GetExamples()
-        {
-            yield return SwaggerExample.Create(
-              "Example", 
-              new RecipientDto
-              {
-                  AccountNumber = "345678",
-                  BankDestinationId = 2,
-                  Id = 4,
-                  SuggestedName = "Pham Van A"
-              }
-            );
-        }
+          }
+        );
+      }
     }
+
+
+    public class CreateRecipientRequestExample : IMultipleExamplesProvider<CreateRecipientDto>
+    {
+      public IEnumerable<SwaggerExample<CreateRecipientDto>> GetExamples()
+      {
+        yield return SwaggerExample.Create(
+          "With Suggested Name",
+          new CreateRecipientDto
+          {
+            AccountNumber = "345678",
+            BankDestinationId = null,
+            SuggestedName = "MyGod",
+          }
+        );
+
+        yield return SwaggerExample.Create(
+           "Without Suggested Name",
+           new CreateRecipientDto
+           {
+             AccountNumber = "345678",
+             BankDestinationId = null,
+             SuggestedName = null,
+           }
+         );
+      }
+    }
+
+    public class GetRecipientRequestDtoExample : IMultipleExamplesProvider<GetRecipientRequestDto>
+    {
+      public IEnumerable<SwaggerExample<GetRecipientRequestDto>> GetExamples()
+      {
+        yield return SwaggerExample.Create(
+                 "Example",
+                 new GetRecipientRequestDto
+                 {
+                   AccountNumber = "324121211",
+                   BankId = 4
+                 }
+               );
+      }
+    }
+
+    public class CreateRecipientResponseExample : IMultipleExamplesProvider<RecipientDto>
+    {
+      public IEnumerable<SwaggerExample<RecipientDto>> GetExamples()
+      {
+        yield return SwaggerExample.Create(
+          "With Suggested Name",
+          new RecipientDto
+          {
+            Id = 1,
+            AccountNumber = "345678",
+            BankDestinationId = null,
+            SuggestedName = "MyGod",
+          }
+        );
+
+        yield return SwaggerExample.Create(
+           "Without Suggested Name",
+           new RecipientDto
+           {
+             Id = 1,
+             AccountNumber = "345678",
+             BankDestinationId = null,
+             SuggestedName = "Nguyen Van B",
+           }
+         );
+      }
+    }
+
+    public IEnumerable<SwaggerExample<RecipientDto>> GetExamples()
+    {
+      yield return SwaggerExample.Create(
+        "Example",
+        new RecipientDto
+        {
+          AccountNumber = "345678",
+          BankDestinationId = 2,
+          Id = 4,
+          SuggestedName = "Pham Van A"
+        }
+      );
+    }
+  }
 }
---
+++ AsrTool/Swagger/Auth/AuthExample.cs
@@ -0,0 +1,85 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using AsrTool.Dtos;
+
+namespace AsrTool.Swagger.Auth
+{
+  public class AuthExample : IMultipleExamplesProvider<LoginRequestDto>
+  {
+
+    public IEnumerable<SwaggerExample<LoginRequestDto>> GetExamples()
+    {
+      yield return SwaggerExample.Create(
+          "Example",
+          new LoginRequestDto
+          {
+            Username = "admin",
+            Password = "password",
+            RecaptchaToken = "string"
+          }
+        );
+    }
+  }
+
+  public class RegisterRequestExample : IMultipleExamplesProvider<RegisterRequestDto>
+  {
+    public IEnumerable<SwaggerExample<RegisterRequestDto>> GetExamples()
+    {
+      yield return SwaggerExample.Create(
+       "Example",
+       new RegisterRequestDto()
+       {
+         Address = "HCM",
+         Email = "employee@gmail.com",
+         FirstName = "Em",
+         LastName = "Ployee",
+         IndentityNumber = "2432513533",
+         Phone = "0341231111",
+         Username = "employee",
+         Password = "password",
+       }
+     );
+    }
+  }
+
+  public class ForgotPasswordGenOTPDtoExample : IMultipleExamplesProvider<ForgotPasswordGenOTPDto>
+  {
+    public IEnumerable<SwaggerExample<ForgotPasswordGenOTPDto>> GetExamples()
+    {
+      yield return SwaggerExample.Create(
+      "Example",
+      new ForgotPasswordGenOTPDto()
+      {
+        Username = "admin"
+      }
+    );
+    }
+  }
+
+  public class ValidateOTPForgotPasswordDtoExample : IMultipleExamplesProvider<ValidateOTPForgotPasswordDto>
+  {
+    public IEnumerable<SwaggerExample<ValidateOTPForgotPasswordDto>> GetExamples()
+    {
+      yield return SwaggerExample.Create(
+       "Example",
+       new ValidateOTPForgotPasswordDto()
+       {
+         Username = "admin",
+         OTP = "243111"
+       });
+    }
+  }
+
+  public class ForgotPasswordDtoExample : IMultipleExamplesProvider<ForgotPasswordDto>
+  {
+    public IEnumerable<SwaggerExample<ForgotPasswordDto>> GetExamples()
+    {
+      yield return SwaggerExample.Create(
+      "Example",
+      new ForgotPasswordDto()
+      {
+        Username = "admin",
+        Password = "password"
+      });
+    }
+  }
+}
