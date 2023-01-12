98d53872d65d8bb3ca01889cc70b6c9231396f26
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2023-01-12 20:49:30 +0700

    Update LoginCommandHandler.cs

--- AsrTool/Infrastructure/MediatR/Businesses/User/Commands/LoginCommandHandler.cs
+++ AsrTool/Infrastructure/MediatR/Businesses/User/Commands/LoginCommandHandler.cs
@@ -23,26 +23,26 @@ namespace AsrTool.Infrastructure.MediatR.Businesses.User.Commands
     {
       if (!Constants.TestAccounts.Contains(request.Request.Username))
       {
-        var dictionary = new Dictionary<string, string> { { "secret", _appSettings.RecaptchaSecretKey }, { "response", request.Request.RecaptchaToken } };
-        var postContent = new FormUrlEncodedContent(dictionary);
-        HttpResponseMessage recaptchaResponse = null;
-        string stringContent = "";
-        using (var http = new HttpClient()) {
-          recaptchaResponse = await http.PostAsync("https://www.google.com/recaptcha/api/siteverify", postContent);
-          stringContent = await recaptchaResponse.Content.ReadAsStringAsync();
-        }
-        if (!recaptchaResponse.IsSuccessStatusCode)
-        {
-          throw new BusinessException("Unable to verify recaptcha token");
-        }
-        if (string.IsNullOrEmpty(stringContent))
-        {
-          throw new BusinessException("Invalid reCAPTCHA verification response");
-        }
-        var googleReCaptchaResponse = JsonConvert.DeserializeObject<ReCaptchaResponse>(stringContent);
-        if (!googleReCaptchaResponse.Success) {
-          throw new BusinessException("Google reCAPTCHA authentication fail");
-        }
+        //var dictionary = new Dictionary<string, string> { { "secret", _appSettings.RecaptchaSecretKey }, { "response", request.Request.RecaptchaToken } };
+        //var postContent = new FormUrlEncodedContent(dictionary);
+        //HttpResponseMessage recaptchaResponse = null;
+        //string stringContent = "";
+        //using (var http = new HttpClient()) {
+        //  recaptchaResponse = await http.PostAsync("https://www.google.com/recaptcha/api/siteverify", postContent);
+        //  stringContent = await recaptchaResponse.Content.ReadAsStringAsync();
+        //}
+        //if (!recaptchaResponse.IsSuccessStatusCode)
+        //{
+        //  throw new BusinessException("Unable to verify recaptcha token");
+        //}
+        //if (string.IsNullOrEmpty(stringContent))
+        //{
+        //  throw new BusinessException("Invalid reCAPTCHA verification response");
+        //}
+        //var googleReCaptchaResponse = JsonConvert.DeserializeObject<ReCaptchaResponse>(stringContent);
+        //if (!googleReCaptchaResponse.Success) {
+        //  throw new BusinessException("Google reCAPTCHA authentication fail");
+        //}
       }
 
       var normalizedUserName = request.Request.Username?.Split(new string[] { "\\", "/" }, StringSplitOptions.RemoveEmptyEntries).LastOrDefault();
